00856ada2f1c90f0e70f9c9dc2be19bec76f5925
Oliver Seemann <oseemann@gmail.com>
2014-03-28 23:22:58 +0100

    use channel instead of locks for cleaner code

--- logger.go
+++ logger.go
@@ -4,55 +4,37 @@ import (
     "os"
     "fmt"
     "time"
-    "sync"
 )
 
-var logbuf1 = make([]LogRecord, 1000)
-var logbuf2 = make([]LogRecord, 1000)
-var curr_ = &logbuf1
-var logbuf_ []LogRecord = (*curr_)[:0]
-var L sync.Mutex
-
 var timestamp_layout = "2006-01-02T15:04:05.00000"
+var log_queue = make(chan LogRecord, 10000)
 
 func log(r LogRecord) {
-    L.Lock()
-    logbuf_ = append(logbuf_, r)
-    L.Unlock()
+    log_queue <- r
 }
 
 func Logger() {
     for ;; {
-        if len(logbuf_) == 0 {
-            time.Sleep(10 * time.Millisecond)
-            continue
-        }
-
-        mybuf := logbuf_
-        // switch buffers
-        if curr_ == &logbuf1 {
-            curr_ = &logbuf2
-            logbuf_ = (*curr_)[:0]
+        n := len(log_queue)
+        if n == 0 {
+            time.Sleep(100 * time.Millisecond)
         } else {
-            curr_ = &logbuf1
-            logbuf_ = (*curr_)[:0]
-        }
-
-        // dump buffer
-        file, err := os.OpenFile("log.txt", os.O_APPEND|os.O_WRONLY, 0660)
-        if err == nil {
-            for _, r := range mybuf {
-                fmt.Fprintf(file,
-                    "%s\t%s\t%s\t%s\t%s\n",
-                    r.ts.Format(timestamp_layout),
-                    r.client,
-                    r.target,
-                    r.referer,
-                    r.useragent)
+            file, err := os.OpenFile("log.txt", os.O_APPEND|os.O_WRONLY, 0660)
+            if err == nil {
+                for i := 0; i < n; i++ {
+                    r := <-log_queue
+                    fmt.Fprintf(file,
+                        "%s\t%s\t%s\t%s\t%s\n",
+                        r.ts.Format(timestamp_layout),
+                        r.client,
+                        r.target,
+                        r.referer,
+                        r.useragent)
+                }
+                file.Close()
+            } else {
+                fmt.Println(err)
             }
-            file.Close()
-        } else {
-            fmt.Println(err) 
         }
     }
 }
