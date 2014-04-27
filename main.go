7f914a8e08d2275aa5d5b2d682ad1db78e6b1579
Oliver Seemann <oseemann@gmail.com>
2014-04-27 21:50:25 +0200

    apply gofmt

--- main.go
+++ main.go
@@ -1,76 +1,76 @@
 package main
 
 import (
-    "fmt"
-    "time"
-    "net/url"
-    "net/http"
+	"fmt"
+	"net/http"
+	"net/url"
+	"time"
 )
 
 type LogRecord struct {
-    ts time.Time
-    client string
-    target string
-    referer string
-    useragent string
+	ts        time.Time
+	client    string
+	target    string
+	referer   string
+	useragent string
 }
 
 var thisHost = "fry:8080"
 
 func root(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hi!")
+	fmt.Fprintf(w, "Hi!")
 }
 
-func make_logrecord(request *http.Request, dest string) (LogRecord) {
+func make_logrecord(request *http.Request, dest string) LogRecord {
 
-    // Referer
-    var referer string = "-"
-    raw_referer := request.Header["Referer"]
-    if len(raw_referer) > 0 {
-        referer = raw_referer[0]
-    }
+	// Referer
+	var referer string = "-"
+	raw_referer := request.Header["Referer"]
+	if len(raw_referer) > 0 {
+		referer = raw_referer[0]
+	}
 
-    // User Agent
-    var useragent string = "-"
-    raw_useragent := request.Header["User-Agent"]
-    if len(raw_useragent) > 0 {
-        useragent = raw_useragent[0]
-    }
+	// User Agent
+	var useragent string = "-"
+	raw_useragent := request.Header["User-Agent"]
+	if len(raw_useragent) > 0 {
+		useragent = raw_useragent[0]
+	}
 
-    logr := LogRecord{
-        time.Now(),
-        request.RemoteAddr,
-        dest,
-        referer,
-        useragent,
-    }
+	logr := LogRecord{
+		time.Now(),
+		request.RemoteAddr,
+		dest,
+		referer,
+		useragent,
+	}
 
-    return(logr)
+	return (logr)
 }
 
 func clickHandler(w http.ResponseWriter, r *http.Request) {
-    raw_dest := r.FormValue("r")
-    dest, err := url.QueryUnescape(raw_dest)
-    if err != nil {
-        // default dest in case of error
-        dest = "http://www.google.com/"
-    }
+	raw_dest := r.FormValue("r")
+	dest, err := url.QueryUnescape(raw_dest)
+	if err != nil {
+		// default dest in case of error
+		dest = "http://www.google.com/"
+	}
 
-    // check sanity of target url
-    // TODO
+	// check sanity of target url
+	// TODO
 
-    // log request
-    logrecord := make_logrecord(r, dest)
-    Log(logrecord)
-    
-    http.Redirect(w, r, dest, 302)
+	// log request
+	logrecord := make_logrecord(r, dest)
+	Log(logrecord)
+
+	http.Redirect(w, r, dest, 302)
 }
 
 func main() {
-    go Logger()
-    http.HandleFunc("/", root)
-    http.HandleFunc("/l", clickHandler)
-    http.ListenAndServe(":8080", nil)
+	go Logger()
+	http.HandleFunc("/", root)
+	http.HandleFunc("/l", clickHandler)
+	http.ListenAndServe(":8080", nil)
 }
 
 // vim: set filetype=go expandtab:
